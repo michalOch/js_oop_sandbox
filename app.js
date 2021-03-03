0cb2884fc41b716ca3314709e457f60567331a10
michalOch <ochrobot1990@gmail.com>
2021-03-03 20:49:11 +0100

    constructor and this keyword

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-alert('Object oriented programming in js');
+// Person constructor
+function Person(name, dob) {
+  this.name = name;
+  // this.age = age
+  this.birthday = new Date(dob);
+
+  this.calculateAge = function () {
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
+}
+
+// const brad = new Person('Brad', 36);
+// const john = new Person('John', 31);
+
+// console.log(brad.age);
+// console.log(john.age);
+
+const brad = new Person('Brad', '9-10-1930');
+console.log(brad.calculateAge());
