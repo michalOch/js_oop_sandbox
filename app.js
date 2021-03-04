29dd7a8498f7157804037887043eb89d45ba2564
michalOch <ochrobot1990@gmail.com>
2021-03-04 15:58:19 +0100

    updated constructor and this keyword

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
+// Object literals
+// const brad = {
+//   name: 'Brad',
+//   age: 30
+// }
+
+// console.log(brad);
+// console.log(brad.age)
+
+// Object constractor
 // Person constructor
-function Person(name, dob) {
-  this.name = name;
-  // this.age = age
+function Person(firstName, lastName, dob) {
+  this.firstName = firstName;
+  this.lastName = lastName;
   this.birthday = new Date(dob);
+
+  this.getFullName = function () {
+    return `User ${this.firstName} ${this.lastName} is ${this.calculateAge()}`;
+  }
 
   this.calculateAge = function () {
     const diff = Date.now() - this.birthday.getTime();
@@ -11,11 +25,19 @@ function Person(name, dob) {
   }
 }
 
-// const brad = new Person('Brad', 36);
-// const john = new Person('John', 31);
+// this - window object
+// console.log(this);
+// const brad = new Person('Brad', 19);
+// const john = new Person('John', 45);
+// console.log(`User ${brad.name} is ${brad.age}`);
+// console.log(`User ${john.name} is ${john.age}`);
 
-// console.log(brad.age);
-// console.log(john.age);
+const brad = new Person('Brad', 'Kowalski', '9-10-1990');
+const john = new Person('John', 'Watson', '10-1-1943');
+const greg = new Person('Greg', 'Super', '2-5-1995');
+const clark = new Person('Clark', 'Kent', '11-11-1960');
 
-const brad = new Person('Brad', '9-10-1930');
-console.log(brad.calculateAge());
+console.log(brad.getFullName());
+console.log(john.getFullName());
+console.log(greg.getFullName());
+console.log(clark.getFullName());
