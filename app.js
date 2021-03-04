89a1e56f7fa1b8c209429ac62dec4f2852bc12ca
michalOch <ochrobot1990@gmail.com>
2021-03-04 16:43:06 +0100

    build in constructor examples

--- app.js
+++ app.js
@@ -1,43 +1,70 @@
-// Object literals
-// const brad = {
-//   name: 'Brad',
-//   age: 30
-// }
+// Strings
 
-// console.log(brad);
-// console.log(brad.age)
+const name1 = 'Jeff';
+const name2 = new String('Jeff');
+// name2.foo = 'bar';
 
-// Object constractor
-// Person constructor
-function Person(firstName, lastName, dob) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.birthday = new Date(dob);
+// console.log(name1);
+// console.log(name2);
 
-  this.getFullName = function () {
-    return `User ${this.firstName} ${this.lastName} is ${this.calculateAge()}`;
-  }
+console.log(typeof name1);
+console.log(typeof name2);
 
-  this.calculateAge = function () {
-    const diff = Date.now() - this.birthday.getTime();
-    const ageDate = new Date(diff);
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
-  }
+if (name2 === 'Jeff') {
+  console.log('yes');
+} else {
+  console.log('no');
 }
 
-// this - window object
-// console.log(this);
-// const brad = new Person('Brad', 19);
-// const john = new Person('John', 45);
-// console.log(`User ${brad.name} is ${brad.age}`);
-// console.log(`User ${john.name} is ${john.age}`);
+// Numbers
+const num1 = 5;
+const num2 = new Number(5);
+console.log(num2);
+console.log(typeof num2);
 
-const brad = new Person('Brad', 'Kowalski', '9-10-1990');
-const john = new Person('John', 'Watson', '10-1-1943');
-const greg = new Person('Greg', 'Super', '2-5-1995');
-const clark = new Person('Clark', 'Kent', '11-11-1960');
+// Boolean
+const bool1 = true;
+const bool2 = new Boolean(true);
+console.log(bool2);
+console.log(typeof bool2);
 
-console.log(brad.getFullName());
-console.log(john.getFullName());
-console.log(greg.getFullName());
-console.log(clark.getFullName());
+// Functions
+const getSum1 = function (x, y) {
+  return x + y;
+}
+
+const getSum2 = new Function('x', 'y', 'return 1 + 2');
+
+
+console.log(getSum1(1, 2));
+console.log(typeof getSum1(1, 2));
+
+console.log(getSum2());
+console.log(typeof getSum2());
+
+// Objects
+const john1 = {
+  name: 'john'
+};
+
+const john2 = new Object({
+  name: 'John2'
+});
+
+console.log(john1);
+console.log(john2);
+
+// Arrays
+const arr1 = [1, 2, 3, 4];
+const arr2 = new Array(1, 2, 3, 4);
+
+console.log(arr1);
+console.log(arr2);
+
+
+// Regular expressions constractor
+const reg1 = /\w+/;
+const reg2 = new RegExp('\\w+');
+
+console.log(reg1);
+console.log(reg2);
